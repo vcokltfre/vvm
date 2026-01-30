9154c352148415be09e3eea0e410bc28cd990767
vcokltfre <vcokltfre@gmail.com>
2026-01-30 14:54:25 +0000

    feat: add label optimiser + command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use vvm::{Program, VM, parse};
 
-const USAGE: &str = "Usage: vvm <run|build|disasm> <infile> [outfile]";
+const USAGE: &str = "Usage: vvm <run|build|disasm|optimise> <infile> [outfile]";
 
 fn run(file: &String) {
     let data = std::fs::read(file).expect("Failed to read input file");
@@ -9,6 +9,18 @@ fn run(file: &String) {
 
     vm.register_native_handler("debug", |vm| {
         vm.debug();
+    });
+
+    vm.register_native_handler("print", |vm| {
+        let value = vm.pop();
+        match value {
+            vvm::Value::Int(i) => print!("{}", i),
+            vvm::Value::String(s) => print!("{}", s),
+            vvm::Value::Bool(b) => print!("{}", b),
+            vvm::Value::Float(f) => print!("{}", f),
+            vvm::Value::UInt(f) => print!("{}", f),
+            _ => println!("<complex value>"),
+        }
     });
 
     vm.run();
@@ -25,6 +37,15 @@ fn disasm(file: &String, outfile: &String) {
     let prog = Program::from_bytecode(data).expect("Failed to parse bytecode");
     let disassembled = vvm::disasm(prog);
     std::fs::write(outfile, disassembled).expect("Failed to write output file");
+}
+
+fn optimise(file: &String, outfile: &String) {
+    let data = std::fs::read(file).expect("Failed to read input file");
+    let prog = Program::from_bytecode(data).expect("Failed to parse bytecode");
+    let optimised_prog = vvm::optimise(&prog);
+    let disassembled = vvm::disasm(optimised_prog);
+    let bytecode = parse(&disassembled).expect("Failed to parse source representation");
+    std::fs::write(outfile, bytecode).expect("Failed to write output file");
 }
 
 fn main() {
@@ -64,6 +85,16 @@ fn main() {
                 }
             };
             disasm(&infile, &outfile);
+        }
+        "optimise" => {
+            let outfile = match std::env::args().nth(3) {
+                Some(f) => f,
+                None => {
+                    eprintln!("{}", USAGE);
+                    return;
+                }
+            };
+            optimise(&infile, &outfile);
         }
         _ => {
             eprintln!("{}", USAGE);
--- src/optimiser.rs
+++ src/optimiser.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
+
 use crate::{Instruction, Program};
 
 pub fn optimise(program: &Program) -> Program {
     let mut instructions = vec![];
-    let labels = program.labels.clone();
 
     let mut prev_was_store_imm = false;
     let mut prev_store_imm_name = String::new();
@@ -11,17 +12,17 @@ pub fn optimise(program: &Program) -> Program {
         instructions.push(instr.clone());
 
         match instr {
-            Instruction::StoreImm (name) => {
+            Instruction::StoreImm(name) => {
                 prev_was_store_imm = true;
                 prev_store_imm_name = name.clone();
             }
-            Instruction::LoadImm (name) if prev_was_store_imm && *name == prev_store_imm_name => {
+            Instruction::LoadImm(name) if prev_was_store_imm && *name == prev_store_imm_name => {
                 instructions.pop(); // Remove the LoadImm
                 instructions.pop(); // Remove the StoreImm
                 prev_was_store_imm = false;
 
                 instructions.push(Instruction::Dup);
-                instructions.push(Instruction::StoreImm (prev_store_imm_name.clone()));
+                instructions.push(Instruction::StoreImm(prev_store_imm_name.clone()));
             }
             _ => {
                 prev_was_store_imm = false;
@@ -29,5 +30,39 @@ pub fn optimise(program: &Program) -> Program {
         }
     }
 
-    Program { instructions, labels }
+    let mut names = HashMap::<String, usize>::new();
+    let mut labels = HashMap::<String, usize>::new();
+
+    for label in program.labels.clone() {
+        let li = names.len();
+        names.insert(label.0.clone(), li);
+        labels.insert(format!("{}", li), label.1);
+    }
+
+    let mut final_instructions = vec![];
+
+    for instr in &instructions {
+        match instr {
+            Instruction::Jump(l) => {
+                let target = names.get(l).expect("Label not found");
+                final_instructions.push(Instruction::Jump(format!("{}", target)));
+            }
+            Instruction::JumpIf(l) => {
+                let target = names.get(l).expect("Label not found");
+                final_instructions.push(Instruction::JumpIf(format!("{}", target)));
+            }
+            Instruction::Call(l) => {
+                let target = names.get(l).expect("Label not found");
+                final_instructions.push(Instruction::Call(format!("{}", target)));
+            }
+            _ => {
+                final_instructions.push(instr.clone());
+            }
+        }
+    }
+
+    Program {
+        instructions: final_instructions,
+        labels,
+    }
 }
